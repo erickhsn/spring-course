7ab5422da3071a448f0c6dd05e98b5f5ed428aee
Erick Nunes <erick.nunes@cedrotech.com>
2018-05-09 10:12:49 -0300

    Projeto inicial, primeiro commit para o diretorio.

---
+++ src/test/java/com/ericknunes/ericknunes/EricknunesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ericknunes.ericknunes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EricknunesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
