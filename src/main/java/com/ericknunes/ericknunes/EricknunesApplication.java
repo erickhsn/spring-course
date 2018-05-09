7af0b33fce6375c01237dd300671f4b1666c5db1
Erick Nunes <erick.nunes@cedrotech.com>
2018-05-09 13:16:35 -0300

    Adicionado dependencias jquery e bootstrap no maven.

--- src/main/java/com/ericknunes/ericknunes/EricknunesApplication.java
+++ src/main/java/com/ericknunes/ericknunes/EricknunesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EricknunesApplication {
+public class 	EricknunesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EricknunesApplication.class, args);
